77055f7e06ba909fbe6e83ea52eb5b0a5b31c4b8
Umer-siddique <sheikhumersiddique1999@gmail.com>
2025-02-25 21:44:50 +0500

    changes in config env

--- src/config/env/develoment.js
+++ src/config/env/develoment.js
@@ -5,7 +5,7 @@ module.exports = {
   port: process.env.PORT || 8000,
   db: {
     mongo_local_uri: process.env.LOCAL_MONGO_URI,
-    mongo_uri: process.env.LOCAL_MONGO_URI,
+    mongo_uri: process.env.MONGO_URI,
     redis_host: process.env.REDIS_HOST,
     redis_port: process.env.REDIS_PORT,
     redis_password: process.env.REDIS_PASSWORD, // Optional, if using Redis with password
